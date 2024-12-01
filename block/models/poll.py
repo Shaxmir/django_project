8b2486e236e59256a553b2448d53864f88518f2c
SoDDoS <shaxrussian@gmail.com>
2024-12-01 11:59:53 +0300

    Модели для опросов

---
+++ block/models/poll.py
@@ -0,0 +1,16 @@
+from tkinter.constants import CASCADE
+
+from django.db import models
+from django.core.validators import MinLengthValidator, MaxValueValidator, MinValueValidator
+
+class Poll(models.Model):
+    title = models.CharField(max_length=50, validators=[MinLengthValidator(10)])
+    description = models.TextField(max_length=500, validators=[MinLengthValidator(50)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Option(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=CASCADE, related_name='options')
+    text = models.CharField(max_length=100, validators=[MinLengthValidator(10)])
+    votes = models.IntegerField(default=0)
